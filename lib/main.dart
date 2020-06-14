e8392deae09b48034be42a935dc9d649865e17ad
MrRobot420 <max.karlchen@web.de>
2020-06-14 18:39:06 +0200

    splitted app widgets | added condition for finished screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_guide/answer.dart';
-import 'package:quiz_guide/question.dart';
+import 'package:quiz_guide/quiz.dart';
+import 'package:quiz_guide/result.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,7 +15,7 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   void _answerQuestion() {
-    if (_questionIndex < questions.length - 1) {
+    if (_questionIndex < _questions.length) {
       setState(() {
         _questionIndex++;
       });
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
     print(_questionIndex);
   }
 
-  var questions = [
+  final _questions = const [
     {
       'questionText': 'Was ist deine Lieblingsfarbe?',
       'answers': ['Rot', 'Gelb', 'Blau', 'Grün']
@@ -45,15 +45,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Meine erste App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_questionIndex]['questionText']),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(answer, _answerQuestion);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                questionIndex: _questionIndex,
+                answerQuestion: _answerQuestion,
+              )
+            : Result(),
       ),
     );
   }
